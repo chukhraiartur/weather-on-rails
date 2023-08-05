190f18cec3171827db71d0d0bf66c9c1b1dbb8d5
Chukhrai Artur <chukhraiartur@gmail.com>
2023-08-05 05:09:14 +0300

    update(routes): added route to clear results of all cities

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  # resources :locations, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "home#index"
+  get '/clear_results', to: 'home#clear_results', as: 'clear_results'
 end
